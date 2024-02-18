1be39ccf8062b29bdcecbb005ad9cf6f0cb90ca7
irinaisheree <irinastoyanova@abv.bg>
2024-02-18 14:56:03 +0200

    added static dashboard page

---
+++ src/controllers/stoneController.js
@@ -0,0 +1,13 @@
+const router = require('express').Router()
+
+// const courseManager = require('../managers/courseManager')
+// const {getErrorMessage} = require('../utils/errorUtils')
+// const {isAuth} = require('../middlewares/authMiddleware')
+
+
+router.get('/dashboard', (req, res) => {
+    res.render('dashboard')
+})
+
+
+module.exports = router
--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,12 @@
 const router = require('express').Router()
 
 const homeController = require('./controllers/homeController')
-// const movieController = require('./controllers/movieController')
+const stoneController = require('./controllers/stoneController')
 // const castController = require("./controllers/castController")
 // const userController = require('./controllers/userController')
 
 router.use(homeController)
-// router.use(movieController)
+router.use(stoneController)
 // router.use('/cast', castController)
 // router.use('/auth', userController)
 
