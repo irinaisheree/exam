e7d0725ca2e9725ca118def4120714576e2c5a99
irinaisheree <irinastoyanova@abv.bg>
2024-02-18 14:59:27 +0200

    add static search page

--- src/controllers/stoneController.js
+++ src/controllers/stoneController.js
@@ -1,12 +1,16 @@
 const router = require('express').Router()
 
 // const courseManager = require('../managers/courseManager')
-// const {getErrorMessage} = require('../utils/errorUtils')
+const {getErrorMessage} = require('../utils/errorUtils')
 // const {isAuth} = require('../middlewares/authMiddleware')
 
 
 router.get('/dashboard', (req, res) => {
     res.render('dashboard')
+})
+
+router.get('/search', (req, res) => {
+    res.render('search')
 })
 
 
